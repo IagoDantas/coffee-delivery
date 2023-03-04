726cee435e4d5dcdeb6a307c5b6fcb44f4e1959f
Iago Dantas <iagodantas.8@gmail.com>
2023-03-03 22:57:21 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  :focus{
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem ${(props) => props.theme['yellow-dark']};
+  }
+
+  body{
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body,
+  input,
+  button,
+  textarea{
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+`
