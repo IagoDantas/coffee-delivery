44b9662928cb1b06c790ebe4992907d00d84592b
Iago Dantas <iagodantas.8@gmail.com>
2023-03-05 21:53:34 -0300

    Checkout altered to Order

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
-import { Checkout } from './pages/Checkout'
+import { Order } from './pages/Order'
 import { DefaultLayout } from './layouts/DefaultLayout'
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order" element={<Order />} />
       </Route>
     </Routes>
   )
