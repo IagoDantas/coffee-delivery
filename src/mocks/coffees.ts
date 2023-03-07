f40245391e0f4c3c261b667d4d7fa766ea5f79c2
Iago Dantas <iagodantas.8@gmail.com>
2023-03-06 23:20:11 -0300

    coffee mock adjusted

---
+++ src/mocks/coffees.ts
@@ -0,0 +1,10 @@
+export const coffees = [
+  {
+    id: '3fb7d801-8bea-4bf4-ab5b-811585c0ace7',
+    name: 'Expresso Tradicional',
+    price: 9.9,
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    tags: ['Tradicional'],
+    imagePath: '../../../../assets/expresso.png',
+  },
+]
